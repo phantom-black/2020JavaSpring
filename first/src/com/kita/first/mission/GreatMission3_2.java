ac8544371ed683b8c7f17fcaf9a268565f1c3860
phantom-black <black_phantom@naver.com>
2020-06-22 17:12:01 +0900

    20200622 자바 복습

---
+++ first/src/com/kita/first/mission/GreatMission3_2.java
@@ -0,0 +1,22 @@
+package com.kita.first.mission;
+
+public class GreatMission3_2 {
+	/*
+		{90, 100, 88},	// 영어 
+		{34, 99, 45},	// 수학
+		{98, 65, 87},	// 국어
+		{100, 88, 10}	// 사회
+		
+		합계, 평균 / 전체합계, 전체평균
+	 */
+	public static void main(String[] args) {
+		int[][] arr = 	{ {90, 100, 88},
+						{34, 99, 45},
+						{98, 65, 87},
+						{100, 88, 10} };
+		
+		String[] subArr = {"영어", "수학", "국어", "사회"};
+		
+		
+	}
+}
